bf680a2e49da68134099f7d70124339fd39bb1b4
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-11-25 18:49:06 +0700

    added some more functions

--- osu_extractor/GetData.py
+++ osu_extractor/GetData.py
@@ -2,6 +2,7 @@ import os
 from os import listdir
 from os.path import isfile, join
 import shutil
+from termcolor import colored
 
 def getSubFolder(path):
     return [f.path for f in os.scandir(path) if f.is_dir()]
@@ -12,8 +13,16 @@ def getAllItemsInFolder(path):
 def getFolderName(path):
     return path.split("\\")[-1]
 
-def extractFiles(root, itemInsideFolder, type, outputDir):
-    print(f"Extracting {type} files from {root}")
+def extractFiles(root, itemInsideFolder, type, outputDirPlusName):
+    print(colored(f"Extracting {type} files from {root}", "green"))
     for file in itemInsideFolder:
         if file.endswith(type):
-            shutil.copy2(root + "\\" + file, outputDir)
+            shutil.copy2(root + "\\" + file, outputDirPlusName)
+
+def createPathIfNotExist(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def keepCertainListByKeyword(list, keyWord):
+    keyWord = keyWord.lower() # Case insensitive
+    return [item for item in list if keyWord in item.lower()]
