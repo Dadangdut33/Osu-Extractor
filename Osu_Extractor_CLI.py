1125b37e76b24137d8f7a168e39d2eb0f413c8c4
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-11-27 08:58:07 +0700

    setting for extract type

--- Osu_Extractor_CLI.py
+++ Osu_Extractor_CLI.py
@@ -121,7 +121,7 @@ class MainProgram:
             print(colored("=" * 70, "blue"))
             print(colored(">> Change Osu! path", "green"))
             print(colored("=" * 70, "blue"))
-            print(colored(">> Press ctrl + c if you want to go back", "blue"))
+            print(colored(">> Press", "blue") + colored(' ctrl + c', 'red') + colored(" to cancel, press", "blue")  + colored(' enter ', 'yellow') + colored("to confirm", "blue"))
             print(colored(">> Input ", "blue") + colored("default", "yellow") + colored(" if you want to use the default path", "blue"))
             print(colored(">> Set:", "blue"))
             print(colored("   Current path\t: ", "yellow") + colored(self.config["osu_path"], "cyan"))
@@ -172,7 +172,7 @@ class MainProgram:
             print(colored("=" * 70, "blue"))
             print(colored(f">> Change output path ({changeType})", "green"))
             print(colored("=" * 70, "blue"))
-            print(colored(">> Press ctrl + c if you want to go back", "blue"))
+            print(colored(">> Press", "blue") + colored(' ctrl + c', 'red') + colored(" to cancel, press", "blue")  + colored(' enter ', 'yellow') + colored("to confirm", "blue"))
             print(colored(">> Input ", "blue") + colored("default", "yellow") + colored(" if you want to use the default path", "blue"))
             print(colored(">> Set:", "blue"))
             print(colored("   Current path\t: ", "yellow") + colored(self.getOutputPath(self.config["output_path"][changeType], changeType), "cyan"))
@@ -193,6 +193,87 @@ class MainProgram:
                 jsonHandler.writeSetting(self.config)
                 print(colored(">> Successfully set ", "green") + colored(self.getOutputPath(self.config["output_path"][changeType], changeType), "yellow") + colored(" as the output path!", "green"))
                 print(colored("Press any key to continue...", "cyan"), end="", flush=True)
+                getch()
+                break
+
+    def changeExtractType(self):
+        insideMenu = True
+        while insideMenu:
+            clearScreen()
+            print(colored("=" * 70, "blue"))
+            print(colored(">> Change extract type", "green"))
+            print(colored("=" * 70, "blue"))
+            print(colored(">> Press", "blue") + colored(" esc ", "red") + colored("if you want to go back", "blue"))
+            print(colored(">> Enable/Disable:", "blue"))
+            print(colored(f"    1. Mp3 (.mp3) (Y)", "green") if self.config['default_extract']['song'] else colored("    1. Mp3 (.mp3) (N)", "red"))
+            print(colored(f"    2. Image (.jpg) (Y)", "green") if self.config['default_extract']['img'] else colored("    2. Image (.jpg) (N)", "red"))
+            print(colored(f"    3. Video (.avi) (Y)", "green") if self.config['default_extract']['video'] else colored("    3. Video (.avi) (N)", "red"))
+            print(colored(f"    4. Custom (Y)", "green") if self.config['default_extract']['custom'] else colored("    4. Custom (N)", "red"))
+            print(colored("[~] 5. Custom extract list:", "blue"))
+            print("    ", end="", flush=True)
+            for item in self.config["default_extract"]["custom_list"]:
+                print(colored(item, "green" if self.config['default_extract']['custom'] else "red"), end=" ")
+            print()
+            print(colored(">> ", "yellow"), end="", flush=True)
+
+            while True:
+                ch = ord(getch())
+                if ch == 27: # ESC
+                    insideMenu = False
+                    break
+                
+                if ch == 49: # 1
+                    self.config['default_extract']['song'] = not self.config['default_extract']['song']
+                    break
+                elif ch == 50: # 2
+                    self.config['default_extract']['img'] = not self.config['default_extract']['img']
+                    break
+                elif ch == 51: # 3
+                    self.config['default_extract']['video'] = not self.config['default_extract']['video']
+                    break
+                elif ch == 52: # 4
+                    self.config['default_extract']['custom'] = not self.config['default_extract']['custom']
+                    break
+                elif ch == 53: # 5
+                    self.askForCustomExtractList()
+                    break
+
+        # Save setting
+        jsonHandler.writeSetting(self.config)
+
+    def askForCustomExtractList(self):
+        strCustom = ""
+        for item in self.config["default_extract"]["custom_list"]:
+            strCustom += item + " "
+
+        strCustom = strCustom.strip()
+
+        while True:
+            clearScreen()
+            print(colored("=" * 70, "blue"))
+            print(colored(">> Press", "blue") + colored(' esc ', 'red') + colored("to cancel, press", "blue")  + colored(' enter ', 'yellow') + colored("to confirm", "blue"))
+            print(colored(">> Input the file format with dot and separated by space, ex: .mp3 .jpg", "green"))
+            print(colored("   Set\t: ", "yellow"))
+            print("   ", end="", flush=True)
+            print(colored(strCustom, "green" if self.config['default_extract']['custom'] else "red"), end="", flush=True)
+
+            ch = ord(getch())
+            # if backspace delete 1
+            if ch == 8:
+                strCustom = strCustom[:-1]
+            else:
+                strCustom += chr(ch)                
+
+            if ch == 27: # ESC
+                break
+            if ch == 13: # enter
+                self.config["default_extract"]["custom_list"] = strCustom.strip().split(" ") if strCustom.strip() != "" else [strCustom]
+
+                if self.config["default_extract"]["custom_list"] == ['\r']:
+                    self.config["default_extract"]["custom_list"] = []
+
+                print(colored("\n>> Successfully set \"", "green") + colored(strCustom.strip(), "yellow") + colored("\" as the custom extract list!", "green"))
+                print(colored("   Press any key to continue...", "cyan"), end="", flush=True)
                 getch()
                 break
 
@@ -240,7 +321,7 @@ class MainProgram:
             self.printCurrentSetting()
 
             print(colored("=" * 70, "blue"))
-            print(colored(">> Press esc key if you want to go back", "blue"))
+            print(colored(">> Press", "blue") + colored(" esc ", "red") + colored("if you want to go back", "blue"))
             print(colored(">> Options:", "blue"))
             print(colored("  1. Extract All Beatmap", "white"))
             print(colored("  2. Extract Certain Beatmap", "white"))
@@ -276,12 +357,12 @@ class MainProgram:
         print(colored(f"    2. Image (.jpg) (Y)", "green") if self.config['default_extract']['img'] else colored("    2. Image (.jpg) (N)", "red"))
         print(colored(f"    3. Video (.avi) (Y)", "green") if self.config['default_extract']['video'] else colored("    3. Video (.avi) (N)", "red"))
         print(colored(f"    4. Custom (Y)", "green") if self.config['default_extract']['custom'] else colored("    4. Custom (N)", "red"))
-        if self.config['default_extract']['custom']:
-            print(colored("[~] Custom extract list", "blue") + self.colon)
-            print("    ", end="", flush=True)
-            for item in self.config["custom_extract_list"]:
-                print(colored(item, "green"), end=", ")
-            print("\b \b")
+        print(colored("[~] Custom extract list", "blue") + self.colon)
+        print("    ", end="", flush=True)
+        for item in self.config["default_extract"]["custom_list"]:
+            print(colored(item, "green" if self.config['default_extract']['custom'] else "red"), end=" ")
+        print()
+
 
     def menuSetting(self):
         insideMenu = True
@@ -295,7 +376,7 @@ class MainProgram:
             self.printCurrentSetting()
 
             print(colored("=" * 50, "blue"))
-            print(colored(">> Press esc key if you want to go back", "blue"))
+            print(colored(">> Press", "blue") + colored(" esc ", "red") + colored("if you want to go back", "blue"))
             print(colored(">> Options:", "blue"))
             print(colored("  1. Change Osu! path", "white"))
             print(colored("  2. Change Output path (song)", "white"))
